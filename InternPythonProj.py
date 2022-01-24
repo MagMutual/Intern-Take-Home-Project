12f68ce9e6a993de5b1897bf4ebf96a6b30dc1aa
William Parish <williamparish159@gmail.com>
2022-01-24 07:41:10 -0600

    Adding Python File

---
+++ InternPythonProj.py
@@ -0,0 +1,13 @@
+def double_list(lst):
+    """
+    Double each element in a list.
+    """
+    for ind in range(lst):
+        lst[ind] = lst[ind] * 2
+    return lst
+
+
+first_list = [1, 2, 3]
+second_list = double_list(first_list)
+
+print(f"Double each element in {first_list} and get {second_list}")
